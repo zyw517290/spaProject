c3d23a7312245dcb8be38c95b4338c47e381befb
zyw517290 <2523929851@qq.com>
2017-06-12 21:27:28 +0800

    锚点的应用

---
+++ src/js/page/pageHome.js
@@ -0,0 +1,37 @@
+var $ = require("jquery");
+var swi = require("../lib/swiper.min.js");
+$(function(){
+	var foodPage = {
+		foodInit:function(){
+			this.foodSlide();
+			this.foodTab();
+		},
+	//	首页轮播图
+		foodSlide:function(){
+			var swiper = new Swiper('.swiper-container', {
+		        loop: true,
+		        pagination: '.swiper-pagination',
+		        paginationClickable: true,
+		        nextButton: '.swiper-button-next',
+	        	prevButton: '.swiper-button-prev'
+		    });
+		},
+	//	底部tab切换
+		foodTab:function(){
+			$(".js-food .js-food-list").eq(0).show().siblings().hide()
+			$(".js-food-footer-anchor").on("click",function(){
+	////			每个页面的tab切换
+				$(this).addClass("food-footer-house-active").siblings().removeClass("food-footer-house-active");
+				$(".js-food .js-food-list").eq($(this).index()).show().siblings().hide();
+	//			spa的应用
+	//			获取当前对象的下标
+				var index = $(this).index(); 
+				var para = $(this).data("anchor");
+				var tabUrl = location.href+"#"+para;
+				location.hash = "#" + para;
+			})
+		}
+	}
+	foodPage.foodInit();
+})
+
---
+++ src/js/release/index.js
@@ -0,0 +1,4 @@
+require("../../css/page/pageHome.css");
+require("../../css/lib/reset.css");
+require("../../css/lib/swiper.min.css");
+require("../page/pageHome.js");
